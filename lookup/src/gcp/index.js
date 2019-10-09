ed9e446bdd7a3d0f1ad03926007db054c31dcbd1
Guillem Mateos <bbguillem@gmail.com>
2019-10-09 18:28:36 +0200

    Change module.yaml to function.yaml to support v0.2

---
+++ lookup/src/gcp/index.js
@@ -0,0 +1,31 @@
+const simplejson = require('@project-furnace/simplejsonutils');
+
+const logic = require('./logic');
+
+const lookupMap = new Map();
+
+const lookupField = process.env.LOOKUP_FIELD;
+const outputLookupField = process.env.OUTPUT_LOOKUP_FIELD;
+
+async function handler(event, context = null) {
+  // check if the event has value defined for what needs to be looked up for
+  const valueToLookup = simplejson.getPath(event, lookupField);
+
+  if (valueToLookup) {
+    // if it does, try and figure out if we already have it in the cache
+    const lookupValue = lookupMap.get(valueToLookup);
+    if (lookupValue) {
+      simplejson.setPath(event, outputLookupField, lookupValue);
+    } else {
+      // check value in the relevant table
+      const value = await logic.getLookupValue(valueToLookup);
+      if (value) {
+        lookupMap.set(valueToLookup, value);
+        event.setPath(event, outputLookupField, value);
+      }
+    }
+  }
+  return event;
+}
+
+module.exports.handler = handler;
